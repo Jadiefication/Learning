da719c54cc502d45c53fbefee6d537cbba527699
Jadiefication <143262080+Jadiefication@users.noreply.github.com>
2023-12-13 20:01:31 +0000

    Dec 13, 2023, 9:01 PM

--- 100 Day challange.py
+++ 100 Day challange.py
@@ -1,7 +1,20 @@
-import random
-while True:
-    try:
-        lenght = int(input("How long do you want the password to be: "))
-    except ValueError:
-        print("Please enter a valid number")
-lenght = len
+
+def mergesort(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+#write me tests
+
+def test_mergesort():
+    assert mergesort([1,2,3,4,5,6,7,8,9,10], [1,2,3,4,5,6,7,8,9,10]) == [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
+    assert mergesort([1,2,3,4,5,6,7,8,9,10], [1,2,3,4,5,6,7,8,9,10]) == [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10]
