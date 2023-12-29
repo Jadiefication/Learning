9253f3f41c8d3871449b369c95b2d9b20aa41383
Jadiefication <143262080+Jadiefication@users.noreply.github.com>
2023-12-29 22:27:21 +0000

    Dec 29, 2023, 11:27 PM

--- 100 Day challange.py
+++ 100 Day challange.py
@@ -4,13 +4,24 @@ class App:
   def __init__(self):
     self.root = tk.Tk()
     
-    self.text = tk.label(self.root, text="Your message")
+    self.text = tk.Label(self.root, text="Your message")
     self.text.pack()
     
     self.textbox = tk.Text(self.root, height=10, width=30)
     self.textbox.pack()
     
-    self.check = tk.Checkbutton(self.root, text="Check me")
+    self.check_state = tk.IntVar()
+    
+    self.check = tk.Checkbutton(self.root, text="Check me", variable=self.check_state, command=self.show_message)
     self.check.pack()
     
+    self.button = tk.Button(self.root, text="OK")
+    self.button.pack()
+    
     self.root.mainloop()
+  
+  def show_message(self):
+    print("Hello World")
+    
+App()
+    
