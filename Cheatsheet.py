2cfb8ab9a84e3507e84a6dad483960d9ca57e5e3
Jadiefication <143262080+Jadiefication@users.noreply.github.com>
2024-01-01 13:37:20 +0000

    Jan 1, 2024, 2:37 PM

--- Cheatsheet.py
+++ Cheatsheet.py
@@ -1,63 +1,5 @@
-import tkinter as tk
-from tkinter import messagebox
-from typing import Any
+# CustomTkinter
 import customtkinter
-
-class App:
-  
-  def __init__(self):
-    
-    self.root = tk.Tk()
-    
-    self.menubar = tk.Menu(self.root)
-    
-    self.filemenu = tk.Menu(self.menubar, tearoff=0)
-    self.filemenu.add_command(label="Close", command=self.on_closing)
-    self.filemenu.add_separator()
-    self.filemenu.add_command(label="Exit", command=exit)
-    
-    self.menubar.add_cascade(menu=self.filemenu, label="File")
-    
-    self.root.config(menu=self.menubar)
-    
-    self.text = tk.Label(self.root, text="Your message")
-    self.text.pack()
-    
-    self.textbox = tk.Text(self.root, height=10, width=30)
-    self.textbox.bind("<KeyPress>", self.key_press)
-    self.textbox.pack()
-    
-    self.check_state = tk.IntVar()
-    
-    self.check = tk.Checkbutton(self.root, text="Check me", variable=self.check_state)
-    self.check.pack()
-    
-    self.button = tk.Button(self.root, text="OK", command=self.show_message)
-    self.button.pack()
-    
-    self.clearbutton = tk.Button(self.root, text="Clear", command=self.clear)
-    self.clearbutton.pack()
-    
-    self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
-    
-    self.root.mainloop()
-  
-  def show_message(self):
-    if self.check_state.get() == 0:
-      print(self.textbox.get("1.0", "end-1c"))
-    else:
-      messagebox.showinfo(title="Message", message=self.textbox.get("1.0", "end-1c"))
-      
-  def key_press(self, event):
-    if event.state == 12 and event.keysym == "Return":
-      self.show_message()
-      
-  def on_closing(self):
-    if messagebox.askyesno(title="Quit?", message="Are you sure you want to quit?"):
-      self.root.destroy()
-      
-  def clear(self):
-    self.textbox.delete("1.0", "end")
     
 #TODO:Make a calculator
 
@@ -71,12 +13,12 @@ class Calculator:
       self.textbox.config(text=current_text + str(number))
   
   def button(self, text: str, row: int, column: int, anchor: str="center", width: int=10, height: int=1, command: any=None):
-    button = tk.Button(self.rt, text=text, width=width, height=height, command=command, font="Arial 12 bold", background="dark gray", foreground="white")
+    button = customtkinter.Button(self.rt, text=text, width=width, height=height, command=command, font="Arial 12 bold", background="dark gray", foreground="white")
     button.grid(row=row, column=column, sticky=anchor)
   
   def __init__(self):
     
-    self.rt = tk.Tk()
+    self.rt = customtkinter.CTk()
     
     customtkinter.set_appearance_mode("System")
     customtkinter.set_default_color_theme("blue")
@@ -85,7 +27,7 @@ class Calculator:
     
     self.rt.title("Calculator")
     
-    self.textbox = tk.Label(self.rt, height=1, width=20, text="0", font="Arial 12 bold")
+    self.textbox = customtkinter.Label(self.rt, height=1, width=20, text="0", font="Arial 12 bold")
     self.textbox.grid(columnspan=4, column=0, row=0, sticky="nsew")
     
     for i in range(0, 4):
