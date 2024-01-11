ccf3ef100ad69779fd21c42121cfec91ef5aa72e
Jadiefication <143262080+Jadiefication@users.noreply.github.com>
2024-01-11 19:19:36 +0000

    Jan 11, 2024, 8:19 PM

---
+++ Dc bot/main.py
@@ -0,0 +1,7 @@
+#bot token MTE2NzgyNjU4MjYwMjI2NDY1Ng.GUdO7I.1WKohb8Ue-P0Tjrkru7u9B8RHXZeuL1la-jINY
+#invite like https://discord.com/api/oauth2/authorize?client_id=1167826582602264656&permissions=377957128192&scope=bot
+
+import bot
+
+if __name__ == "__main__":
+    bot.run_discord_bot()
