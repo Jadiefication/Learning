b5989c61b55212e5a8f1e69585ec2730aabf0a85
Jadiefication <143262080+Jadiefication@users.noreply.github.com>
2024-01-11 19:23:55 +0000

    Jan 11, 2024, 8:23 PM

---
+++ Dc bot/bot.py
@@ -0,0 +1,19 @@
+import discord
+import responses
+
+async def send_message(message, user_message, is_private):
+    try:
+        responses = responses.handle_response(user_message)
+        await message.author.send(responses) if is_private else await message.channel.send(responses)
+    except Exception as e:
+        print(e)
+        
+def run_discord_bot():
+    TOKEN = "MTE2NzgyNjU4MjYwMjI2NDY1Ng.GUdO7I.1WKohb8Ue-P0Tjrkru7u9B8RHXZeuL1la-jINY"
+    client = discord.Client()
+    
+    @client.event
+    async def on_ready():
+        print(f"{client.user} has connected to Discord!")
+        
+    client.run(TOKEN)
