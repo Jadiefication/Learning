a16bf3055ec62871c42b15aa6b78650fbc73d327
Jadiefication <143262080+Jadiefication@users.noreply.github.com>
2024-01-11 19:24:43 +0000

    Jan 11, 2024, 8:24 PM

--- Dc bot/bot.py
+++ Dc bot/bot.py
@@ -1,6 +1,9 @@
 import discord
 import responses
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 async def send_message(message, user_message, is_private):
     try:
         responses = responses.handle_response(user_message)
@@ -10,7 +13,7 @@ async def send_message(message, user_message, is_private):
         
 def run_discord_bot():
     TOKEN = "MTE2NzgyNjU4MjYwMjI2NDY1Ng.GUdO7I.1WKohb8Ue-P0Tjrkru7u9B8RHXZeuL1la-jINY"
-    client = discord.Client()
+    client = discord.Client(intents=intents)
     
     @client.event
     async def on_ready():
